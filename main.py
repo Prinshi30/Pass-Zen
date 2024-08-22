9e5696ca348c3e43407bb3b5705e15adb8f86fc1
Priyanshi singh <95485651+Prinshi30@users.noreply.github.com>
2024-08-22 13:40:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,60 @@
-
-try:
-    from tkinter import *
-except ImportError:
-    from Tkinter import *
-import time
-from pwgenfunc import RandPass
+from tkinter import *
+import string
+import random
 import pyperclip
 
-# Function to generate a password and update the UI
-def pwGenerator(size=8):
-    data = RandPass(size)
-    new_password = data[0]
-    pw_strength = data[1]
-    pw_color = data[2]
 
-    # Update the password entry field and label with the new password and its strength
-    PASSWORD.set(new_password)
-    label_strength.configure(foreground="white", background=pw_color, text=pw_strength, font=('Segoe UI', 10, 'bold'), bd=10, height=1, width=10)
-    
-    # Copy the new password to the clipboard
-    gui.clipboard_clear()
-    gui.clipboard_append(new_password)
-    gui.update()
-    time.sleep(0.02)
-    gui.update()
+def generator():
+    small_alphabets=string.ascii_lowercase
+    capital_alphabets=string.ascii_uppercase
+    numbers=string.digits
+    special_charecters=string.punctuation
 
-# Main window setup
-gui = Tk()
-gui.title("Password Generator")
-gui.config(bg='#1A1A1A')
-width = 600
-height = 342
-screen_width = gui.winfo_screenwidth()
-screen_height = gui.winfo_screenheight()
-x = (screen_width / 2) - (width / 2)
-y = (screen_height / 2) - (height / 2)
-gui.geometry(f"{width}x{height}+{int(x)}+{int(y)}")
+    all=small_alphabets+capital_alphabets+numbers+special_charecters
+    password_length=int(length_Box.get())
 
-# Variables
-PASSWORD = StringVar()
-PW_SIZE = IntVar()
-PW_SIZE.set(8)  # Default password size
+    if choice.get()==1:
+        passwordField.insert(0,random.sample(small_alphabets,password_length))
 
-# Frame setup
-Top = Frame(gui, width=width, bg='#1A1A1A')
-Top.pack(side=TOP)
+    if choice.get()==2:
+        passwordField.insert(0,random.sample(small_alphabets+capital_alphabets,password_length))
 
-Form = Frame(gui, width=width, bg="#1A1A1A")
-Form.pack(side=TOP)
-
-Bot = Frame(gui, width=width)
-Bot.pack(side=BOTTOM)
-
-# UI Elements
-label_password = Label(Form, font=('Segoe UI', 18), text="Password", foreground="white", background="#1A1A1A", bd=10)
-label_password.grid(row=0, pady=10)
-
-label_strength = Label(Form, font=('Segoe UI', 10, 'bold'), foreground="white", background="white", text="Weak", bd=10, height=1, width=10)
-label_strength.grid(row=0, column=3, pady=10, padx=10)
-
-label_pw_size = Label(Form, font=('Segoe UI', 18), text="Size", foreground="white", background="#1A1A1A", bd=10)
-label_pw_size.grid(row=2, pady=10)
-
-label_instructions = Label(Bot, width=width, font=('Segoe UI', 12, 'bold'), text="Password Generated to your Clipboard!", foreground="white", background="#1A1A1A", bd=1, relief=SOLID)
-label_instructions.pack(fill=X)
-
-# Widgets
-password = Entry(Form, textvariable=PASSWORD, font=(18), width=24)
-password.grid(row=0, column=1, columnspan=2)
-
-pw_size = Scale(Form, from_=8, to=24, length=200, width=24, sliderlength=14, orient=HORIZONTAL, variable=PW_SIZE, foreground="white", background="#1A1A1A", font=(16))
-pw_size.grid(row=2, column=1, columnspan=2)
-
-# Function to copy the generated password to the clipboard
-def Copy_password():
-    pyperclip.copy(PASSWORD.get())
-
-# Buttons
-Button(Top, text='COPY TO CLIPBOARD', foreground="white", background="#1A1A1A", command=Copy_password).pack(pady=5)
-
-btn_generate = Button(Form, text="Generate Now", width=20, command=lambda: pwGenerator(PW_SIZE.get()))
-btn_generate.grid(row=4, column=1, columnspan=2)
-
-# Run the main event loop
-gui.resizable(False, False)
-gui.mainloop()
+    if choice.get()==3:
+        passwordField.insert(0,random.sample(all,password_length))
 
 
+def copy():
+    random_password=passwordField.get()
+    pyperclip.copy(random_password)
+
+root=Tk()
+root.config(bg='gray20')
+choice=IntVar()
+Font=('arial',13,'bold')
+passwordLabel=Label(root,text='Password Generator',font=('times new roman',20,'bold'),bg='gray20',fg='white')
+passwordLabel.grid(pady=10)
+weakradioButton=Radiobutton(root,text='Weak',value=1,variable=choice,font=Font)
+weakradioButton.grid(pady=5)
+
+mediumradioButton=Radiobutton(root,text='Medium',value=2,variable=choice,font=Font)
+mediumradioButton.grid(pady=5)
+
+strongradioButton=Radiobutton(root,text='Strong',value=3,variable=choice,font=Font)
+strongradioButton.grid(pady=5)
+
+lengthLabel=Label(root,text='Password Length',font=Font,bg='gray20',fg='white')
+lengthLabel.grid(pady=5)
+
+length_Box=Spinbox(root,from_=5,to_=18,width=5,font=Font)
+length_Box.grid(pady=5)
+
+generateButton=Button(root,text='Generate',font=Font,command=generator)
+generateButton.grid(pady=5)
+
+passwordField=Entry(root,width=25,bd=2,font=Font)
+passwordField.grid()
+
+copyButton=Button(root,text='Copy',font=Font,command=copy)
+copyButton.grid(pady=5)
+
+root.mainloop()
